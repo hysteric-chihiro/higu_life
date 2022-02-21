bd32861bdd8501629b5f9c0739dd099a7531d27f
hysteric-chihiro <100114252+hysteric-chihiro@users.noreply.github.com>
2022-02-21 17:22:06 +0900

    Add files via upload

---
+++ My_Portfolio/Higu Tech/main.js
@@ -0,0 +1,36 @@
+$(function(){
+    $(".hamburger").on("click", function(){
+        hamburger();
+    });
+
+    $("#navi a").on("click", function(){
+        hamburger();
+    });
+
+    $('.inview-slide-left').on('inview', function(event, isInView, visibleX, visibleY){
+        $(this).stop().addClass('slide-left');
+    });
+    
+    $('.inview-slide-right').on('inview', function(event, isInView, visiblePartX, visiblePartY){
+        if (isInView){
+            $(this).stop().addClass('slide-right');
+        }
+    });
+
+    $('.inview-balloon').on('inview', function(event, isInView, visiblePartX, visiblePartY){
+        if(isInView) {
+            $(this).stop().addClass('balloon');
+        }
+    });
+});
+
+
+function hamburger() {
+    $(".hamburger").toggleClass('active');
+
+    if ($('.hamburger').hasClass('active')) {
+        $('#navi').addClass("active");
+    } else {
+        $("#navi").removeClass("active");
+    }
+}
