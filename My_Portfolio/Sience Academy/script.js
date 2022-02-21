94f803bebda6b5b4ab570873c7cdd2f253a17b4d
hysteric-chihiro <100114252+hysteric-chihiro@users.noreply.github.com>
2022-02-21 17:26:28 +0900

    Add files via upload

---
+++ My_Portfolio/Sience Academy/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.toggle_btn').on('click', function() {
+        if($('#header').hasClass('open')) {
+            $('#header').removeClass('open');
+        } else {
+            $('#header').addClass('open');
+        }
+    });
+
+    $('#mask').on('click',function(){
+        $('#header').removeClass('open');
+    });
+
+    $('#navi a').on('click',function(){
+        $('#header').removeClass('open')
+    })
+
+    $('.welcome').fadeIn(1500);
+
+});
